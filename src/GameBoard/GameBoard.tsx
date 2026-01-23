baab755bcc4ed1860ebdb57e608071ca3a53d790
Logan White <135063791+LWhite49@users.noreply.github.com>
2026-01-23 14:51:03 -0500

    Integrated cellType into onClick handlers to differentiate between which level is clicked

--- src/GameBoard/GameBoard.tsx
+++ src/GameBoard/GameBoard.tsx
@@ -14,16 +14,52 @@ import testImage from "../assets/testImage.jpg";
 
 // GameBoard instance - renders collection of SingleCells
 const GameBoard = () => {
+	// PLACE INTERNAL STATE HERE
 	// Define Initial Matrix State
-	const [matrix /*setMatrix */] = useState<number[][]>([
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0],
-		[0, 0, 0, 0, 0, 0, 0]
+	const [matrix, setMatrix] = useState<number[][]>([
+		[-1, -1, -1, -1, -1, -1, -1],
+		[-1, 0, 0, 0, 0, 0, -1],
+		[-1, 0, 0, 0, 0, 0, -1],
+		[-1, 0, 0, 0, 0, 0, -1],
+		[-1, 0, 0, 0, 0, 0, -1],
+		[-1, 0, 0, 0, 0, 0, -1],
+		[-1, -1, -1, -1, -1, -1, -1]
 	]);
+
+	// Define State for NextToPlace
+	const [nextToPlace, setNextToPlace] = useState<number>(1);
+
+	// Define Internal State for last cell placed
+	const [lastCellPlaced, setLastCellPlaced] = useState<number[]>([-1, -1]);
+
+	// Define Internal State for which "level" is active
+	const [activeLevel /*setActiveLevel*/] = useState<1 | 2>(1);
+
+	// Define Internal State for Current Score
+	const [score /*setScore*/] = useState<number>(0);
+
+	// PLACE METHODS HERE
+	// Define function for processing a move in lvl 1 - accepts row column coordinates and cell type positionally
+	function processLvl1Move(r: number, c: number, cellType: string): void {
+		// Block clicks on lvl 2 cell clicks for now
+		if (cellType !== "grey") {
+			return;
+		}
+
+		// Very Naive Approach
+		setMatrix((prev) => {
+			prev[r][c] = nextToPlace;
+			return prev;
+		});
+		setNextToPlace((prev) => prev + 1);
+		setLastCellPlaced([r, c]);
+	}
+
+	// Define function for processing a move in lvl2 - accepts row column coordinates positionally
+	// not touching this - just for architecture
+	function processLvl2Move(r: number, c: number, cellType: string): void {
+		console.log(r, c, cellType, "Lvl 2 not yet implemented");
+	}
 
 	// Return Grid of SingleCells, passing corresponding matrix value to each
 	return (
@@ -41,6 +77,8 @@ const GameBoard = () => {
 					bgColor="purple"
 				/>
 			</div>
+			<p className="helperText">Current Score is: {score}</p>
+			<p className="helperText">Next Number to Place is: {nextToPlace}</p>
 			<div className="grid7x7">
 				{matrix.map((row, rowCount) =>
 					row.map((cellValue, cellCount) => {
@@ -49,11 +87,21 @@ const GameBoard = () => {
 							<SingleCell
 								value={cellValue}
 								cellType={getCellType(currLoop)}
+								row={rowCount}
+								column={cellCount}
+								onClick={
+									activeLevel == 1
+										? processLvl1Move
+										: processLvl2Move
+								}
 							/>
 						);
 					})
 				)}
 			</div>
+			<p className="helperText">
+				{`Last Cell (debugging): ${lastCellPlaced}`}
+			</p>
 		</div>
 	);
 };
--- src/SingleCell/SingleCell.tsx
+++ src/SingleCell/SingleCell.tsx
@@ -4,12 +4,19 @@ import "./SingleCell.css";
 interface SingleCellProps {
 	cellType: "grey" | "blue" | "gold";
 	value: number;
+	onClick: (r: number, c: number, cellType: string) => void;
+	row: number;
+	column: number;
 }
 
 // SingleCell instance - displays it's value in a square shape
 const SingleCell = (props: SingleCellProps) => {
 	return (
-		<div className={`singleCell ${props.cellType}`}>
+		<div
+			className={`singleCell ${props.cellType}`}
+			onClick={() =>
+				props.onClick(props.row, props.column, props.cellType)
+			}>
 			<p className={`cellText`}>{props.value}</p>
 		</div>
 	);
